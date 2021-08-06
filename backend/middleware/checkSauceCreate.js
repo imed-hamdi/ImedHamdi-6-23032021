f2a3154efc42ed144a6524019f1a70eff8c95ead
imed-hamdi <imedh00u@gmail.com>
2021-08-06 16:46:54 +0200

    regex

--- backend/middleware/checkSauceCreate.js
+++ backend/middleware/checkSauceCreate.js
@@ -6,18 +6,18 @@ module.exports = (req, res, next) => {
         req.body.sauce = JSON.parse(req.body.sauce)
     }
 
-    const regexDescription = /[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ_!$_\s-]{10,200}/;
-    const regexName = /[a-zA-Z áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]{3,20}/; // a utilisé pour le nom de la sauce et le créateur
+    const regexDescription =/^([a-zA-Z ]){2,100}$/;
+    const regexName = /^([a-zA-Z ]){2,30}$/; // a utilisé pour le nom de la sauce et le créateur
     let testName = regexName.test(req.body.sauce.name)
-    if (!testName) return res.status(400).json('test Name failed') //si ca va pas en stope le script et on return une erreur 
+    if (!testName) return res.status(400).json({ message:'test Name failed'}) //si ca va pas en stope le script et on return une erreur 
 
     let testManufacturer = regexName.test(req.body.sauce.manufacturer)
-    if (!testManufacturer) return res.status(400).json('test Manufacturer failed') //si ca va pas en stope le script et on return une erreur 
+    if (!testManufacturer) return res.status(400).json({ message:'test Manufacturer failed'}) //si ca va pas en stope le script et on return une erreur 
 
  
     
     let testDescription = regexDescription.test(req.body.sauce.description)
-    if (!testDescription) return res.status(400).json('test Name failed')
+    if (!testDescription) return res.status(400).json({ message:'test Name failed'})
 
 
     next() // si tout va bien on passe au prochain middleware 
--- backend/middleware/checkSauceModify.js
+++ backend/middleware/checkSauceModify.js
@@ -15,16 +15,16 @@ module.exports = (req, res, next) => {
 
   //Vérification champs formulaire
  
-  const regexName = /[a-zA-Z áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]{3,20}/; // a utilisé pour le nom de la sauce et le créateur
+  const regexName = /^([a-zA-Z ]){2,30}$/; // a utilisé pour le nom de la sauce et le créateur
   let testName = regexName.test(sauceObjectModify.name)
-  if (!testName) return res.status(400).json('test Name failed') //si ca va pas en stope le script et on return une erreur 
+  if (!testName) return res.status(400).json({ message:'test Name failed'}) //si ca va pas en stope le script et on return une erreur 
 
   let testManufacturer = regexName.test(sauceObjectModify.manufacturer)
-  if (!testManufacturer) return res.status(400).json('test Manufacturer failed') //si ca va pas en stope le script et on return une erreur 
+  if (!testManufacturer) return res.status(400).json({ message:'test Manufacturer failed'}) //si ca va pas en stope le script et on return une erreur 
 
-  const regexDescription = /[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ_!$_\s-]{10,200}/;
+  const regexDescription = /^([a-zA-Z ]){2,100}$/;//
   let testDescription = regexDescription.test(sauceObjectModify.description)
-  if (!testDescription) return res.status(400).json('test Name failed')
+  if (!testDescription) return res.status(400).json({ message:'test Name failed'})
 
   next(); // si tout va bien on passe au prochain middleware 
 };
